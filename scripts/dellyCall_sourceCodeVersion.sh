8d1ef11bbc260e9ef5169232ab9d120ace639b29
Atma Maria Ivancevic <a1211880@l03.phoenix.adelaide.edu.au>
2018-05-22 12:01:32 +0930

    more stuff

---
+++ scripts/dellyCall_sourceCodeVersion.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Script for calling SVs with delly
+## Date: 22 May 2018 
+##
+## Example usage:
+## INDIR=/data/neurogenetics/alignments/Illumina/genomes/allGenomes sbatch --array 0-126 dellyCall_sourceCodeVersion.sh
+
+#SBATCH -A robinson
+#SBATCH --qos=long
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --time=7-00:00
+#SBATCH --mem=32GB
+
+# Notification configuration 
+#SBATCH --mail-type=END                                         
+#SBATCH --mail-type=FAIL                                        
+#SBATCH --mail-user=%u@adelaide.edu.au
+
+# compile delly from source 
+# to incorporate latest commits
+# cd /fast/users/a1211880/GITHUBrepos/
+# git clone --recursive https://github.com/dellytools/delly.git
+# cd delly/
+# module load Boost/1.61.0-foss-2016uofa-Python-2.7.11
+# make all
+
+# define key variables
+export FASTDIR=/fast/users/$USER
+
+DELLYEXE=$FASTDIR/GITHUBrepos/delly/src/delly
+OUTDIR=$FASTDIR/outputs
+GENOMEDIR=$FASTDIR/references/genomes
+
+# define query bam files
+QUERIES=($(ls $INDIR/*.bam | xargs -n 1 basename))
+
+# load modules
+module load BCFtools/1.3.1-GCC-5.3.0-binutils-2.25
+
+# run the thing
+
+### SV discovery/calling phase ###
+echo $(date +"[%b %d %H:%M:%S] Starting delly")
+echo "Processing file: "${QUERIES[$SLURM_ARRAY_TASK_ID]}
+
+$DELLYEXE call \
+-g ${GENOMEDIR}/ucsc.hg19.fasta \
+-o ${OUTDIR}/SVcalling/dellyOut/${QUERIES[$SLURM_ARRAY_TASK_ID]}.bcf \
+-x ${GENOMEDIR}/ucsc.hg19.excl.tsv \
+${INDIR}/${QUERIES[$SLURM_ARRAY_TASK_ID]} \
+2> ${FASTDIR}/slurmLOG/dellyCalling/${QUERIES[$SLURM_ARRAY_TASK_ID]}.log
+
+echo $(date +"[%b %d %H:%M:%S] All done!")
