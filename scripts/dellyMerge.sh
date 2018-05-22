06b5f2c0afbdf6697708ae8ce46c8f8c464b5444
Atma M Ivancevic <atma.ivancevic@adelaide.edu.au>
2018-05-22 16:17:29 +0930

    Update dellyMerge.sh

--- scripts/dellyMerge.sh
+++ scripts/dellyMerge.sh
@@ -31,7 +31,8 @@ cd $INDIR
 pwd
 
 # filter bams to only retain chr1-22,X,Y
-# because different hg19 reference have diff alt contigs
+# because different hg19 references have diff alt contigs
+# and this will make delly merge fail
 echo $(date +"[%b %d %H:%M:%S] Restricting variants to chr1-22,X,Y")
 for i in *.bam.bcf; do bcftools view $i --regions chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY -O b -o ${i%.bam.bcf}.filtered.bcf; done
 
