65d666198ea7211435c9f3669de7ce8e223efcbb
Atma M Ivancevic <atma.ivancevic@adelaide.edu.au>
2018-05-22 16:15:27 +0930

    Update dellyMerge.sh

--- scripts/dellyMerge.sh
+++ scripts/dellyMerge.sh
@@ -30,10 +30,18 @@ echo $(date +"[%b %d %H:%M:%S] Got to dir")
 cd $INDIR
 pwd
 
+# filter bams to only retain chr1-22,X,Y
+# because different hg19 reference have diff alt contigs
+echo $(date +"[%b %d %H:%M:%S] Restricting variants to chr1-22,X,Y")
+for i in *.bam.bcf; do bcftools view $i --regions chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY -O b -o ${i%.bam.bcf}.filtered.bcf; done
+
+# set the list of bams to merge
 echo $(date +"[%b %d %H:%M:%S] Set bcf list")
-bcfList=$(find *.bcf)
+bcfList=$(find *filtered.bcf)
 echo $bcfList
 
+# merge bams
+# n increases the max SV size to 100000000
 echo $(date +"[%b %d %H:%M:%S] Merge all bcfs")
 $DELLYEXE merge -n 100000000 -o sites.bcf $bcfList
 
