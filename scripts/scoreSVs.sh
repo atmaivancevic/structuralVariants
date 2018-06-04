4af352305611790b828886bc68b0f581729c7b8d
Atma M Ivancevic <atma.ivancevic@adelaide.edu.au>
2018-06-04 11:20:31 +0930

    scoring time

---
+++ scripts/scoreSVs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Score SVs for potential pathogenicity
+## Date: 4 June 2018 
+##
+## Example usage: ./scoreSVs.sh
+
+# remove chr perfix (for SVscore, which cannot have prefix)
+cat germline.vcf | sed 's/chr//g' > germline_noPrefix.vcf
+
+# use SVscore to add scores and gene exon/intron overlaps
+svscore.pl -dv -e ~/Documents/Atma/references/genes/refGene.exons.bed -f ~/Documents/Atma/references/genes/refGene.introns.bed -c ~/Documents/Atma/references/snvs/whole_genome_SNVs.tsv.gz -i germline_noPrefix.vcf > germline_noPrefix_scored.vcf
